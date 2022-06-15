0e46c653467c5cf87432ba4997b098727051eb1e
Laura Conde <laurusmayer@gmail.com>
2022-06-15 22:08:12 +0200

    Laura 4

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
+        System.out.println("Introduce numerito: ");
         int numerito= sc.nextInt();
 
     }
